d02bbf9b2423e46a6eef85a6a11663e371d505a3
joannabpark <joannabpark1@gmail.com>
2021-03-04 11:15:00 -0600

    more php practice

--- index.php
+++ index.php
@@ -1,37 +1,18 @@
 <?php 
 
-        $ninjas = ['shaun', 'ryu', 'yoshi'];
+    // echo true; "1"
+    // echo false; ""
 
-        // for ($i = 0; $i < count($ninjas); $i++) {
-        //     echo $ninjas[$i] . '<br/>';
-        // };
+    // echo 5 < 10;
+    // echo 5 > 10;
+    // echo 5 == 10;
+    // echo 10 == 10;
+    // echo 5 != 10;
+    // echo 5 <= 5;
+    // echo 5 >= 10;
 
-        // foreach($ninjas as $ninja) {
-        //     echo $ninja . '<br/>';
-        // };
-
-        
-        // foreach($products as $product) {
-            //     echo $product['name'] . ' - ' . $product['price'];
-            //     echo '<br/>';
-            // };
-            
-            // $i = 0;
-            
-            // while($i < count($products)){
-                //     echo $products[$i]['name'];
-                //     echo '<br/>';
-                //     $i++;
-                // }
-                
-                $products = [
-                    ['name'=> 'shiny star', 'price'=> 20],
-                    ['name'=> 'green shell', 'price'=> 10],
-                    ['name'=> 'red shell', 'price'=> 15],
-                    ['name'=> 'gold coin', 'price'=> 5],
-                    ['name'=> 'lightning bolt', 'price'=> 50],
-                    ['name'=> 'banana skin', 'price'=> 3]
-                ];
+    
+    
 ?>
 
 <!DOCTYPE html>
@@ -40,16 +21,6 @@
     <title>my first PHP file</title>
 </head>
 <body>
-
-    <h1>Products</h1>
-    <ul>
-        <?php foreach($products as $product){ ?>
-
-            <h3><?php echo $product['name']; ?></h3>
-            <p>$ <?php echo $product['price']; ?></p>
-
-        <?php } ?>
-    </ul>
 
 </body>
 </html>
